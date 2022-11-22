945c28eef770e10ce7130fea2ed29ba7a7fb3368
fatimaqais <fatima_qais@hotmail.com>
2022-11-22 19:54:35 +0000

    Add questions for quiz

---
+++ assets/js/question.js
@@ -0,0 +1,102 @@
+// questions for the quiz
+
+const quizQuestions = [{
+    question: "In the Disney story, 'The Little Mermaid' which of the following is not Trition's daughter?",
+    answers: [
+        {option: 'Andrina'},
+        {option: 'Attina'},
+        {option: 'Adora'},
+        {option: 'Allana'}
+    ],
+    correctAnswer: 'Adora'
+},
+{
+    question: "Which one of the following is the famous dialogue of the evil queeen from Snow White?",
+    answers: [
+        {option: "Mirror mirror on the wall who's the fairest of them all?"},
+        {option: "Mirror mirror on the wall who's the fairest one of all?"},
+        {option: "Mirror mirror on the wall who's the fairest out of all?"},
+        {option: "Mirror mirror on the wall who's fairest of them all?"}
+    ],
+    correctAnswer: "Mirror mirror on the wall who's the fairest one of all?"
+},
+{
+    question: "What had Cinderella left behind at the ball?",
+    answers: [
+        {option: "Ring"},
+        {option: "Glass Shoe"},
+        {option: "Scarf"},
+        {option: "Tiara"}
+    ],
+    correctAnswer: "Glass Shoe"
+},
+{
+    question: "How many brothers did the character Hans have in Frozen?",
+    answers: [
+        {option: "5"},
+        {option: "10"},
+        {option: "12"},
+        {option: "6"}
+    ],
+    correctAnswer: "12"
+},
+{
+    question: "For how many years did the Genie in Aladdin say, he got trapped in the lamp?",
+    answers: [
+        {option: "100 years"},
+        {option: "10,000 years"},
+        {option: "1000 years"},
+        {option: "500 years"}
+    ],
+    correctAnswer: "10,000 years"
+},
+{
+    question: "What name did Ursula use when she became a human in 'The Little Mermaid'",
+    answers: [
+        {option: "Vanessa"},
+        {option: "Alexis"},
+        {option: "Cathy"},
+        {option: "Bella"}
+    ],
+    correctAnswer: "Venessa"
+},
+{
+    question: "How many dwarfs are there in Snow White?",
+    answers: [
+        {option: "4"},
+        {option: "8"},
+        {option: "7"},
+        {option: "10"}
+    ],
+    correctAnswer: "10"
+},
+{
+    question: "What is the name of the princess in Aladdin?",
+    answers: [
+        {option: "Feline"},
+        {option: "Tink"},
+        {option: "Ella"},
+        {option: "Jasmine"}
+    ],
+    correctAnswer: "Jasmine"
+},
+{
+    question: "Who put Buzz's arm back together?",
+    answers: [
+        {option: "The Mutant Toys"},
+        {option: "Woody"},
+        {option: "Sid"},
+        {option: "Molly"}
+    ],
+    correctAnswer: "The Mutant Toys"
+},
+{
+    question: "What is Peter Pan's sidekick's name",
+    answers: [
+        {option: "Wendy"},
+        {option: "Dinah"},
+        {option: "Alice"},
+        {option: "Tinker Bell"}
+    ],
+    correctAnswer: "Tinker Bell"
+}]
