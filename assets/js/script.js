6ca931707eade939966507b9e67c09cd009b6af7
fatimaqais <fatima_qais@hotmail.com>
2022-11-24 23:54:26 +0000

    add function to display questions in html from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,59 @@
 // getting the DOM elements
-let start = document.getElementById('start-btn');
+let startButton = document.getElementById('start-btn');
 let startPage = document.getElementById('start-container');
 let user= document.getElementById('username');
 let results = document.getElementById('score-page');
 let quizArea = document.getElementById('quiz-container');
-let question = document.getElementById('question-area');
+let displayQuestion = document.getElementById('question-area');
 let answer = document.getElementById('answers');
+let nextBtn = document.getElementById('next')
+let optionA = document.getElementById('answer-a')
+let optionB = document.getElementById('answer-b')
+let optionC = document.getElementById('answer-c')
+let optionD = document.getElementById('answer-d')
 
+let selectedAnswer;
 let currentQuestionIndex;
-let shuffleQuestion;
 
 // start quiz event listener
-start.addEventListener('click', startQuiz);
+startButton = addEventListener('click', startQuiz)
 
 //when start button is clicked
 function startQuiz() {
-    startQuiz.classList.add('display');
-    startPage.classList.add('display');
-    results.classList.add('display');
-    quizArea.classList.remove('display');
-    currentQuestionIndex = 0
-    shuffleQuestion = question.sort(() => Math.random() - .5)
-    showQuestions()
-    console.log('showing questions next')
+    console.log('started')
+    startPage.classList.add('display')
+    currentQuestionIndex = 0;
+    quizArea.classList.remove('display')
+    nextQuestion()
+}
+
+function nextQuestion(){
+    showQuestions(quizQuestions[currentQuestionIndex]);
 }
 
 function showQuestions(question) {
-    question.innerText= quizQuestions.question;
+    displayQuestion.innerText = question.question;
+    optionA.innerText = question.answers[0].option;
+    optionB.innerText = question.answers[1].option;
+    optionC.innerText = question.answers[2].option;
+    optionD.innerText = question.answers[3].option;
+
+    optionA.onclick = pickAnswer;
+    optionB.onclick = pickAnswer;
+    optionC.onclick = pickAnswer;
+    optionD.onclick = pickAnswer
+} 
+
+function pickAnswer(event) {
+    console.log('selected answer')
+    selectedAnswer = e.target;
+    let userAnswer = selectedAnswer.innerText;
+    let rightAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+    if (userAnswer === rightAnswer) {
+        selectedAnswer.classList.add('correct');
+        score();
+        currentQuestionIndex++;
+    }
 }
+
+//function score()
