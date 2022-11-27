7bf412eae171e9dddce26656191097f2a695e976
fatimaqais <fatima_qais@hotmail.com>
2022-11-27 01:46:58 +0000

    Add A function to check users answer and increment score

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,34 @@ let quizArea = document.getElementById('quiz-container');
 let displayQuestion = document.getElementById('question-area');
 let answer = document.getElementById('answers');
 let nextBtn = document.getElementById('next');
+let nextContainer = document.getElementById('next-conatiner')
 let optionA = document.getElementById('answer-a');
 let optionB = document.getElementById('answer-b');
 let optionC = document.getElementById('answer-c');
 let optionD = document.getElementById('answer-d');
 let progressBar = document.getElementById('progress-bar');
 let playAgainBtn = document.getElementById('play-again');
-let questionCounter = document.getElementById('question-counter')
+let questionCounter = document.getElementById('question-counter');
+let score = document.getElementById('score');
 
-let selectedAnswer;
 let currentQuestionIndex = 0;
 let questionNumber = 1;
+let userScore = 0;
 
 // start quiz event listener
-startButton = addEventListener('click', startQuiz)
+startButton.onclick = () => {
+    startQuiz()
+}
 
 //when start button is clicked
 function startQuiz() {
     console.log('started')
     startPage.classList.add('display')
     quizArea.classList.remove('display')
-    if (currentQuestionIndex <= '9') {
-        console.log('showing question')
-        questionCounter.innerText = questionNumber;
-    } else {
-        results.classList.remove('display')
-        quizArea.classList.add('display')
-        console.log('showing result')
-        return (currentQuestionIndex)
-    }
+
     showQuestions(quizQuestions[currentQuestionIndex]);
+    questionCounter.innerText = questionNumber;
+    score.innerText = userScore;
 }
 
 
@@ -47,27 +45,47 @@ function showQuestions(question) {
     optionC.innerText = question.answers[2].option;
     optionD.innerText = question.answers[3].option;
 
-    let selectedChoice = [
-        optionA.onclick = pickAnswer,
-        optionB.onclick = pickAnswer,
-        optionC.onclick = pickAnswer,
-        optionD.onclick = pickAnswer
-    ]
+    optionA.onclick = pickAnswer;
+    optionB.onclick = pickAnswer;
+    optionC.onclick = pickAnswer;
+    optionD.onclick = pickAnswer;
 }
 
-function pickAnswer(option) {
-    selectedAnswer = option.textContent
-    console.log(selectedAnswer)
+function pickAnswer(event) {
+//    optionA.setAttribute('disabled', 'disabled');
+//    optionB.setAttribute('disabled', 'disabled');
+//    optionC.setAttribute('disabled', 'disabled');
+//    optionD.setAttribute('disabled', 'disabled'); 
+    let selectedAnswer = event.target;
+    let chosenAnswer = selectedAnswer.innerText;
+    console.log(chosenAnswer)
+    let rightAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+    if (chosenAnswer === rightAnswer) {
+        userScore += 2;
+        showQuestions(quizQuestions[currentQuestionIndex]);
+        console.log('correct answer')
+    } else {
+        showQuestions(quizQuestions[currentQuestionIndex]);
+        console.log('wrong answer')
+    }
+    showQuestions(quizQuestions[currentQuestionIndex]);
 }
 
 // when next button is clicked
 nextBtn.onclick = () => {
     currentQuestionIndex++;
     questionNumber++;
-}
-
-function score() {
-
+    if (currentQuestionIndex <= '9') {
+        console.log('showing question')
+        questionCounter.innerText = questionNumber;
+    } else {
+        results.classList.remove('display')
+        quizArea.classList.add('display')
+        console.log('showing result')
+        return (currentQuestionIndex)
+    }
+    showQuestions(quizQuestions[currentQuestionIndex]);
+    score.innerText = userScore;
 }
 
 playAgainBtn.onclick = () => {
